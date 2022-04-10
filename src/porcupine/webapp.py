1c6f8c3a4018c8ba4f7008e557d7dd65e24c869d
Manuel Bovo <manuel.bovo@gmail.com>
2022-04-10 20:10:23 +0200

    WIP: config and api

--- src/porcupine/webapp.py
+++ src/porcupine/webapp.py
@@ -17,3 +17,28 @@ def health():
     ret = {"status": "OK"}
     code = 200
     return ret, code
+
+
+@web.route("/v1/boot/<string:macaddress>")
+def bootstrap(macaddress: str):
+    rc = 200
+    ret = {"kernel": "", "initrd": [""], "message": "", "cmdline": ""}
+    ret["cmdline"] += f"ip={client_ip}:{server_ip}:{gateway_ip}:{netmask}:{hostname}:{device}:off:{dns_ip}::{ntp_ip}"
+    ret["cmdline"] += f"talos.config={myaddress}/v1/config/"
+    return jsonify(ret), rc
+
+
+@web.route("/v1/config/<string:role>")
+def clusterconfig(role: str):
+    rc = 200
+    return "this is a big config file", rc
+
+
+@web.route("/defaults")
+def defaults():
+    return cfg.defaults, 200
+
+
+@web.route("/mapping")
+def mapping():
+    return cfg.mapping, 200
