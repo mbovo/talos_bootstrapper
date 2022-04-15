50aa0ad3211020c47a0a68985a83ad8b72e4c19b
Manuel Bovo <manuel.bovo@gmail.com>
2022-04-15 12:15:31 +0200

    Fixin test using typer

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,25 +1,34 @@
-from pixiefairy import cli
-from click import testing
+from pixiefairy.cli import cli
+from typer.testing import CliRunner
 import pkg_resources
+from pytest import fixture
 
 
 def test_assert_true():
     assert True
 
 
-def test_main_cli():
-    runner = testing.CliRunner()
-    assert runner.invoke(cli.main, "").exit_code == 0
+@fixture
+def runner():
+    return CliRunner()
 
 
-def test_main_version():
-    runner = testing.CliRunner()
-    result = runner.invoke(cli.main, "version")
+def test_main_cli(runner):
+    assert runner.invoke(cli, "").exit_code == 2
+
+
+def test_main_version(runner):
+    result = runner.invoke(cli, "version")
     assert result.exit_code == 0
     assert result.stdout == f"pixiefairy - Pixiecore API Companion v{pkg_resources.get_distribution('pixiefairy').version}\n"
 
 
-def test_main_start():
-    runner = testing.CliRunner()
-    result = runner.invoke(cli.start)
-    assert result.exit_code == 0
+def test_main_start(runner):
+    result = runner.invoke(cli, "start")
+    assert result.exit_code == 2
+
+
+# # need to mock server.run()
+# def test_main_start_ok(runner):
+#     result = runner.invoke(cli, ["start","-c","./examples/config.yaml"])
+#     assert result.exit_code == 0
